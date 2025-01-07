60ccbde8f7c399330aebd635ee17f6a2a98ce914
sudipX0 <sudippoudel@sd.taylors.edu.my>
2025-01-08 01:02:11 +0545

    Added pagination

--- app/routes.py
+++ app/routes.py
@@ -11,8 +11,9 @@ import os
 @app.route("/")
 @app.route("/home")
 def main():
-    post = Post.query.all()
-    return render_template("index.html", posts=post)
+    page = request.args.get("page", default=1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template("index.html", posts=posts)
 
 
 @app.route("/about")
