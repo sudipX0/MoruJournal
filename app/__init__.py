3921cbfb2266edf7b599ae8b6378e75e1d414b19
sudipX0 <sudippoudel@sd.taylors.edu.my>
2025-01-08 14:27:38 +0545

    added db migrations

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,19 @@ from config import Config
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_mgr = LoginManager(app)
 login_mgr.login_view = "login"
 mail = Mail(app)
+
+
+migrate = Migrate(app, db)
+
 
 from app import routes
